ae46d2732685295f79353a20837b763307adf20d
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-05-16 17:41:37 +0200

    data; add mefstats

--- server.rb
+++ server.rb
@@ -8,3 +8,7 @@ set :logging, true
 get "/list-etabs" do
   send_file "./data/fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre.csv"
 end
+
+get "/list-mefstats" do
+  send_file "./data/en_n_mef_stat4_vu1_1_0-1.csv"
+end
