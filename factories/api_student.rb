e064e925c021880a803bc1ca85016830226b6ee6
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-07-17 23:07:44 +0200

    factories: rename the student factory to api_student
    We use `student` in the root project, so to avoid conflicts yet be
    able to re-use this factory, use a better name.

--- factories/api_student.rb
+++ factories/api_student.rb
@@ -9,13 +9,14 @@ require "ostruct"
 Faker::Config.locale = :fr
 
 FactoryBot.define do
-  factory :student, class: OpenStruct do # rubocop:disable Style/OpenStructUse
+  factory :sygne_student, class: OpenStruct do # rubocop:disable Style/OpenStructUse
     ine { Faker::Number.number(digits: 10) }
     prenom { Faker::Name.first_name }
     nom { Faker::Name.last_name }
     dateNaissance { Faker::Date.between(from: 20.years.ago, to: 16.years.ago) }
     codeSexe { [0, 1].sample }
     niveau { 2122 } # { Faker::Number.within(range: 3000..4000) }
+    mef { }
     libelleNiveau { %w[3EME 2NDE 1ERE].sample }
     classe { %w[3EME2 2NDE6 1EREB 1EREA].sample }
     codeRegime { "" }
