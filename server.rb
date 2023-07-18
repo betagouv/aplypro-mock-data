9868ba403c0b2c71166345ed8921cb4bf62fcf65
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-07-18 13:48:36 +0200

    server: add the etab-info route

--- server.rb
+++ server.rb
@@ -21,6 +21,10 @@ get "/sygne/generated/irrelevant_mefs" do |uai|
   json FactoryBot.build_list(:student, 40, codeUai: uai, niveau: "1111").map(&:to_h)
 end
 
+get "/etab-info/search" do
+  send_file "./data/etab.json"
+end
+
 post "/sygne/token" do
   json(
     expires_in: 600,
