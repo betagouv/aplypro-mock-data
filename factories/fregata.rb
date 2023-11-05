2248cba1ad881dafa1046a6fd2a7e70dbc9c1475
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-11-05 23:33:43 +0100

    sygne: return some data for extended student info

---
+++ factories/fregata.rb
@@ -0,0 +1,67 @@
+require "active_support"
+require "active_support/core_ext/integer/time"
+require "factory_bot"
+require "faker"
+require "ostruct"
+
+# rubocop:disable Metrics/BlockLength
+FactoryBot.define do
+  factory :fregata_student, class: OpenStruct do # rubocop:disable Style/OpenStructUse
+    id { Faker::Number.number }
+    division do
+      {
+        "libelle" => classe_label
+      }
+    end
+    sectionReference do
+      {
+        "codeMef" => "27621407320"
+      }
+    end
+    apprenant do
+      {
+        "prenomUsuel" => Faker::Name.first_name,
+        "nomUsuel" => Faker::Name.last_name,
+        "dateNaissance" => Faker::Date.between(from: 20.years.ago, to: 16.years.ago).to_s,
+        "ine" => ine,
+        "dateSortieEtablissement" => left_at
+      }
+    end
+
+    trait :gone do
+      left_at { Date.yesterday.to_s }
+    end
+
+    trait :irrelevant do
+      sectionReference do
+        {
+          "codeMef" => "123123"
+        }
+      end
+    end
+
+    trait :no_ine do
+      apprenant do
+        {
+          "ine" => nil
+        }
+      end
+    end
+
+    transient do
+      left_at { nil }
+
+      ine { Faker::Number.number(digits: 10).to_s }
+
+      classe_label do
+        [
+          "2NDE JARDINERIE",
+          "2NDE ALIMENTATION BIO",
+          "1ERE CONDUITE PRODUCTION",
+          "1ERE CONSEIL VENTE"
+        ].sample
+      end
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
--- server.rb
+++ server.rb
@@ -18,6 +18,10 @@ get "/sygne/v1/etablissements/*/eleves" do |uai|
   json FactoryBot.build_list(:sygne_student, 40, codeUai: uai, mef: "2212421011").map(&:to_h)
 end
 
+get "/sygne/v1/eleves/*" do |ine|
+  json FactoryBot.build(:sygne_student_info, ine: ine).to_h
+end
+
 get "/sygne/generated/irrelevant_mefs" do |uai|
   json FactoryBot.build_list(:student, 40, codeUai: uai, niveau: "1111").map(&:to_h)
 end
